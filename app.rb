bcf5a318038ecd438f8555ce2f0a867085e8f25d
TONYA JENNINGS <you@example.com>
2017-07-29 23:03:29 -0400

    app.rb edited

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require "sinatra"
 
 get '/' do #when we go to the root, we are going to go to the erb:index
-	erb :index #put in the symbol for the page we want
+	redirect '/user_name'
 
+end
+
+get '/user_name'do
+   erb :index
 end
 
 post '/user_name' do
