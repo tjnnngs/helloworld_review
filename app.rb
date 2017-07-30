979c9c42b06f019c1a0ec7de1e4f4de02cf98dbb
TONYA JENNINGS <you@example.com>
2017-07-30 15:23:54 -0400

    still not passing name through to age page

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require "sinatra"
 
 get '/' do #when we go to the root, we are going to go to the erb:index
 	redirect '/user_name'
-
 end
 
 get '/user_name'do
@@ -11,5 +10,16 @@ end
 
 post '/user_name' do
 	name_provided = params[:input_name]
-	"Hello, #{name_provided}"
+	redirect '/age?name_provided=' + name_provided
 end
+
+get '/age' do
+	name_provided = params[:name_provided]
+	puts "name_provided in get age #{name_provided}"
+	erb :age, :locals => {:name_provided => name_provided}
+end
+
+# post 'age' do
+# 	age_provided = params [:input_age]
+# 	redirect '/fave_nums?user_age' + user_age
+# end
