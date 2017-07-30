cbe5a055eb0eecf3fb2ce981efad3b10ddd0588a
TONYA JENNINGS <you@example.com>
2017-07-29 22:58:38 -0400

    Hello World add name

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require "sinatra"
 
 get '/' do #when we go to the root, we are going to go to the erb:index
-	erb :index
+	erb :index #put in the symbol for the page we want
+
+end
+
+post '/user_name' do
+	name_provided = params[:input_name]
+	"Hello, #{name_provided}"
 end
